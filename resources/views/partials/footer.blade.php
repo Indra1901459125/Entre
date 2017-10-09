66fd04231e847c4892f14eb77ce38c28c4782cb5
ricardSup <richardlim97@rocketmail.com>
2017-10-09 23:22:28 +0700

    Add footer and change non-fixed-top header

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -1,1 +1,36 @@
-footer.blade.php
+<div class="col-md-12 footer">
+<div class="col-md-4 about_scanfcode">
+   <h4>LOGO</h4>
+  <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been  the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley.</p>
+        <p><i class="fa fa-map-pin"></i> 210, Narotam Nagar, Mohali, Punjab-141401,INDIA</p>
+        <p><i class="fa fa-phone"></i> Phone (India) : +91 9876543210</p>
+        <p><i class="fa fa-envelope"></i> E-mail : code@scanfcode.com</p>
+</div>
+<div class="col-md-3 links_scanfcode">
+ <h4>General Links</h4>
+ <ul>
+	<li><a href='#'>Blog</a></li>
+        <li><a href='#'>About Us</a></li>
+	<li><a href='#'>Contact Us</a></li>
+	<li><a href='#'>Terms & Conditions</a></li>
+	<li><a href='#'>Privacy Policy</a></li>
+	<li><a href='#'>Sitemaps</a></li>
+	<li><a href='#'>Frequently asked question</a></li>
+ </ul>
+</div>
+<div class="col-md-5 feedback">
+ <h4>Subscribe</h4>
+ <form>
+ <input type="text" placeholder="Enter your email"/><button type="submit" class="btn btn-primary">SUBSCRIBE</button>
+ <h4 style="margin-top:50px;">Connect+</h4>
+ 
+      <li><a target="_blank" href="https://www.facebook.com/"><i class="fa fa-facebook"></i></a></li>
+      <li><a target="_blank" href="https://twitter.com/"><i class="fa fa-twitter"></i></a></li>
+      <li><a target="_blank" href="https://www.linkedin.com/"><i class="fa fa-linkedin"></i></a></li>
+      <li><a target="_blank" href="https://plus.google.com/"><i class="fa fa-google-plus"></i></a></li>
+      <li><a target="_blank" href="https://www.pinterest.com/"><i class="fa fa-pinterest"></i></a></li>
+</div>
+<div class="col-md-12" style="text-align:center;padding:5px;color:#fff;">
+	© 2017, ServiceKomputer, All rights reserved
+</div>
+</div>
